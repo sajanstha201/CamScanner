9d91620b4a5b0278d85434f0661afc70bc22406f
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-06-17 15:30:48 +0545

    combined the google map and the contact us page

--- src/webpage/AboutUs.js
+++ src/webpage/AboutUs.js
@@ -1,39 +1,13 @@
 import React, { useState } from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
 
 export const AboutUs = () => {
-  const [mapLoaded, setMapLoaded] = useState(false);
 
-  const defaultCenter = {
-    lat: 27.681505372996934,
-    lng: 85.32804964028425
-  };
-
-  const mapStyles = {
-    height: '400px',
-    width: '400px'
-  };
-
-  const handleLoad = () => {
-    setMapLoaded(true);
-    console.log('Map loaded successfully');
-  };
 
   return (
     <>
       <h1>This is the About Us page</h1>
-      <div>
-        <LoadScript googleMapsApiKey="AIzaSyDR-Piy7y9bIfz9HzE_dN_TAXJbM9UtA24">
-          <GoogleMap
-            mapContainerStyle={mapStyles}
-            zoom={15}
-            center={defaultCenter}
-            onLoad={handleLoad}
-          >
-            {mapLoaded && <Marker position={defaultCenter} />}
-          </GoogleMap>
-        </LoadScript>
-      </div>
+
     </>
   );
 };
