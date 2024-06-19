abd67aee8e69e75ecb2642c320cf95e4c50150e1
bikash2007 <bikashbhandari600@gmail.com>
2024-06-19 14:31:50 +0545

    support added

--- src/components/User/Support.js
+++ src/components/User/Support.js
@@ -1,7 +1,21 @@
+
 export const Support=()=>{
     return(
         <>
-        <h1>This is Support section</h1>
+         <div className="w-full  flex flex-col lg:flex-row justify-center">
+              <div className="   flex  flex-col items-start bg-gradient-to-r from-gray-500/40 to-gray-800/40 w-full md:w-80 lg:w-80">
+                 <div className="flex flex-col gap-2 w-full">
+
+                     <div className="flex justify-center gap-2 mt-5  px-8 py-2 backdrop-blur-lg bg-gray-700/40 hover:bg-gray-700/80 items-center h-12 w-full  text-zinc-900 font-light rounded-md text-sm">  <h5>Help center</h5> </div>
+                     <div className="flex justify-center items-start  px-8 py-2 backdrop-blur-lg bg-gray-700/40 hover:bg-gray-700/80 h-12 w-full  text-zinc-900 font-light rounded-md text-sm"> <h5>Support Inbox</h5> </div>
+                     <div className="flex justify-center items-start   px-8 py-2 backdrop-blur-lg bg-gray-700/40 hover:bg-gray-700/80 h-12 w-full  text-zinc-900 font-light rounded-md text-sm"> <h5>Report Problem</h5> </div>
+
+                 </div>
+              </div>
+              <div className="w-full h-screen flex bg-black">
+                          
+              </div>
+         </div>
         </>
     )
 }
