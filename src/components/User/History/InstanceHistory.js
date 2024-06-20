ac4530f6d93aa56930d0d2143f2abd3baf16c8e1
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-06-20 22:43:57 +0545

    added hover download in history section

--- src/components/User/History/InstanceHistory.js
+++ src/components/User/History/InstanceHistory.js
@@ -1,7 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faDownload, faL } from "@fortawesome/free-solid-svg-icons"
+import { useState } from "react"
 export const InstanceHistory=({instanceHistoryData})=>{
+    const [isHover,setIsHover]=useState(false)
     return(
-        <div className="border  bg-white hover:bg-gray-500 shadow-md rounded-md w-[100px] h-[130px] m-3 relative ">
-            {instanceHistoryData}
+        <div className={`border ${isHover?'bg-gray-400':'bg-white'} shadow-md rounded-md w-[120px] h-[120px] m-3 flex items-center justify-center`}
+        onMouseEnter={()=>{setIsHover(true)}}
+        onMouseLeave={()=>{setIsHover(false)}}>
+            <div className={`${isHover?'hidden':'flex'}`}>
+                {instanceHistoryData}
+            </div>
+            <div className={`${isHover?'flex':'hidden'}`}>
+                <FontAwesomeIcon icon={faDownload}/>
+            </div>
+
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+  purge: [
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust paths according to your project structure
+    './public/index.html',
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
