f90f60df447b0e3aad7e480ff990651b29251cd3
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-06-20 11:59:32 +0545

    added the one day history

--- src/components/User/History/HistoryPage.js
+++ src/components/User/History/HistoryPage.js
@@ -1,10 +1,19 @@
 import { OneDayHistory } from "./OneDayHistory"
 
 export const HistoryPage=()=>{
+    const historyDict={
+        1:{1:['sajan','shrerstha'],2:['country','nepal'],3:['city','kathmandu']},
+        2:{1:['any','kumar'],2:['country','india'],3:['city','banglore']}
+    }
     return(
-        <>
-        <h1>This is history section</h1>
-        <OneDayHistory/>
-        </>
+        <div className="flex flex-col items-center min-h-[80vh] p-2 bg-gray-100 ">
+            <h1>History</h1>
+            <div className="flex flex-col w-[70%] ">
+            {Object.keys(historyDict).map((key)=>(
+                <OneDayHistory key={key} oneDayData={historyDict[key]}/>
+            ))}
+            </div>
+        </div>
+
     )
 }
--- src/components/User/History/InstanceHistory.js
+++ src/components/User/History/InstanceHistory.js
@@ -1,7 +1,9 @@
 export const InstanceHistory=()=>{
     return(
         <>
-        <h1>This is instane History</h1>
+        <div className="border border-black-400 bg-white shadow-md rounded-md w-[100px] h-[130px] m-3">
+
+        </div>
         </>
     )
 }
