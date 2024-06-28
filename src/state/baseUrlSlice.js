fe8d2cefd84f7627ed87349602746929b6853f03
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-06-28 17:49:20 +0545

    changed the base url

--- src/state/baseUrlSlice.js
+++ src/state/baseUrlSlice.js
@@ -3,8 +3,8 @@ import { createSlice } from "@reduxjs/toolkit";
 export const baseUrlSlice=createSlice({
     name:'base_url',
     initialState:{
-        // 'backend':'http://192.168.1.140:8000/',
-        'backend':'http://192.168.1.65:8000/',
+        'backend':'http://192.168.1.140:8000/',
+        // 'backend':'http://192.168.1.65:8000/',
         'frontend':'http://localhost:3000/'
     },
     reducers:{
