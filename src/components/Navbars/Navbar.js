a01cef194f5edebef49eb280424f8cb96094d9ab
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-07-01 16:14:21 +0545

    made responseive for mobile version

--- src/components/Navbars/Navbar.js
+++ src/components/Navbars/Navbar.js
@@ -7,7 +7,9 @@ import { faEnvelope, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { Navbar as NavbarB,Nav,Container } from 'react-bootstrap';
 import { AiOutlineFilePdf, AiOutlineTable, AiOutlineFileText, AiOutlinePicture } from 'react-icons/ai';
 import { NavItemWithIcon } from './NavItemWithIcon';
+import { useMediaQuery } from 'react-responsive';
 export const Navbar=()=>{
+    const isMobile = useMediaQuery({ query: '(max-width: 1000px)' });
     const userInfo=useSelector((state)=>state.userProfile)
     const loc=useLocation()
     const path=['/login','/register']
@@ -27,20 +29,36 @@ export const Navbar=()=>{
                     className="d-inline-block align-top"
                 />{' '}
             </NavbarB.Brand>
-            <NavbarB.Toggle aria-controls="basic-navbar-nav" />
-            <NavbarB.Collapse id="basic-navbar-nav" >
-                <Nav className="me-auto" >
-                    <NavItemWithIcon to={'/pdf-conversion'} icon={<AiOutlineFilePdf/>} currentPath={loc}>Pdf Conversion</NavItemWithIcon>
-                    {userInfo.isLogin&&(<>
-                        <NavItemWithIcon to={'/table-extraction'} icon={<AiOutlineTable/>} currentPath={loc}>Table Extraction</NavItemWithIcon>
-                        <NavItemWithIcon to={'/document-analysis'} icon={<AiOutlineFileText/>} currentPath={loc}>Document Analysis</NavItemWithIcon>
-                        <NavItemWithIcon to={'/image-conversion'} icon={<AiOutlinePicture/>} currentPath={loc}>ImageConversion</NavItemWithIcon>
+            {isMobile&&<>
+                    {!userInfo.isLogin && (
+                    <Nav className='flex flex-row gap-4'>
+                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                        <Nav.Link as={Link} to="/register">Sign Up</Nav.Link>
+                    </Nav>
+                )}
+                {userInfo.isLogin && (
+                    <>
+                    <UserNavbar></UserNavbar>
                     </>
                     )}
-                <NavItemWithIcon to={'/about-us'} icon={<FontAwesomeIcon icon={ faInfoCircle} />} currentPath={loc}>About Us</NavItemWithIcon>
-                <NavItemWithIcon to={'/contact-us'} icon={<FontAwesomeIcon icon={faEnvelope} />} currentPath={loc}>Contact Us</NavItemWithIcon>
-                </Nav>
-                {!userInfo.isLogin && (
+                </>}
+
+            {!isMobile&&<>
+                    <NavbarB.Toggle aria-controls="basic-navbar-nav" />
+                <NavbarB.Collapse id="basic-navbar-nav" >
+                    <Nav className="me-auto" >
+                        <NavItemWithIcon to={'/pdf-conversion'} icon={<AiOutlineFilePdf/>} currentPath={loc}>Pdf Conversion</NavItemWithIcon>
+                        {userInfo.isLogin&&(<>
+                            <NavItemWithIcon to={'/table-extraction'} icon={<AiOutlineTable/>} currentPath={loc}>Table Extraction</NavItemWithIcon>
+                            <NavItemWithIcon to={'/document-analysis'} icon={<AiOutlineFileText/>} currentPath={loc}>Document Analysis</NavItemWithIcon>
+                            <NavItemWithIcon to={'/image-conversion'} icon={<AiOutlinePicture/>} currentPath={loc}>ImageConversion</NavItemWithIcon>
+                        </>
+                        )}
+                    <NavItemWithIcon to={'/about-us'} icon={<FontAwesomeIcon icon={ faInfoCircle} />} currentPath={loc}>About Us</NavItemWithIcon>
+                    <NavItemWithIcon to={'/contact-us'} icon={<FontAwesomeIcon icon={faEnvelope} />} currentPath={loc}>Contact Us</NavItemWithIcon>
+                    </Nav>
+                </NavbarB.Collapse>
+                    {!userInfo.isLogin && (
                     <Nav>
                         <Nav.Link as={Link} to="/login">Login</Nav.Link>
                         <Nav.Link as={Link} to="/register">Sign Up</Nav.Link>
@@ -51,7 +69,7 @@ export const Navbar=()=>{
                     <UserNavbar></UserNavbar>
                     </>
                     )}
-            </NavbarB.Collapse>
+                </>}
         </Container>
     </NavbarB>
     )
--- src/webpage/Home.js
+++ src/webpage/Home.js
@@ -2,7 +2,9 @@ import tableExtractionImage from '../static/images/tableExtraction.png'
 import pdfConversionImage from '../static/images/pdfConversion.png'
 import documentAnalysisImage from '../static/images/documentAnalysis.png'
 import { Link } from 'react-router-dom'
+import { useMediaQuery } from 'react-responsive';
 export const Home=()=>{
+    const isMobile = useMediaQuery({ query: '(max-width: 1000px)' });
     return(
         <>
     <div class="h-52 w-full flex flex-col justify-center items-center text-center p-4">
@@ -13,17 +15,17 @@ export const Home=()=>{
         <Link to='/pdf-conversion'  class="no-underline bg-white h-100  w-[50%] md:w-[25%] rounded-lg border-grey-700 border shadow-sm shadow-zinc-700 p-4 flex flex-col justify-center items-center hover:shadow-cyan-500/50">
             <img class="h-20" src={pdfConversionImage} alt="PDF Conversion"></img>
             <h1 class="font-bold text-2xl">PDF Conversion</h1>
-            <p class="text-zinc-700 text-sm md:text-base">Use our PDF converter to turn Microsoft Word documents, Excel spreadsheets, and PowerPoint files into PDFs. You can also convert PDFs back to their original formats.</p>
+            {!isMobile&&<p class="text-zinc-700 text-sm md:text-base">Use our PDF converter to turn Microsoft Word documents, Excel spreadsheets, and PowerPoint files into PDFs. You can also convert PDFs back to their original formats.</p>}
         </Link>
         <Link to='/table-extraction' class="no-underline bg-white h-100  w-[50%]  md:w-[25%] rounded-lg border-zinc-900 border shadow-sm shadow-zinc-700 p-4 flex flex-col justify-center items-center hover:shadow-cyan-500/50">
             <img class="h-20" src={tableExtractionImage} alt="Table Extraction"></img>
             <h1 class="font-bold text-2xl">Table Extraction</h1>
-            <p class="text-zinc-700 text-sm md:text-base">Extract data tables from PDFs and save results to CSV, XLS, XLSX, and other formats. With this tool, you can get any statistical data in a convenient format for processing.</p>
+            {!isMobile&&<p class="text-zinc-700 text-sm md:text-base">Extract data tables from PDFs and save results to CSV, XLS, XLSX, and other formats. With this tool, you can get any statistical data in a convenient format for processing.</p>}
         </Link>
         <Link  to='/document-analysis' class="no-underline bg-white h-100 w-[50%]  md:w-[25%] rounded-lg border-zinc-900 border shadow-sm shadow-zinc-700 p-4 flex flex-col justify-center items-center hover:shadow-cyan-500/50">
             <img class="h-20" src={documentAnalysisImage} alt="Document Analysis"></img>
             <h1 class="font-bold text-2xl">Document Analysis</h1>
-            <p class="text-zinc-700 text-sm md:text-base">Our innovative, user-friendly, and reliable document analysis tool offers intelligent document conversions, OCR, full API, and workflow automation as per your needs.</p>
+            {!isMobile&&<p class="text-zinc-700 text-sm md:text-base">Our innovative, user-friendly, and reliable document analysis tool offers intelligent document conversions, OCR, full API, and workflow automation as per your needs.</p>}
         </Link>
     </div>
         </>
